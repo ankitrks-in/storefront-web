99c91fa3a5cb7b9b8a2ed02da10152763336db84
Ubuntu <ubuntu@ip-172-31-9-199.ap-south-1.compute.internal>
2022-04-23 11:53:06 +0000

    server changes commit

--- wp-content/themes/mine/header.php
+++ wp-content/themes/mine/header.php
@@ -10,12 +10,18 @@
  */
 
 ?>
+
+<?php header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Pragma: no-cache');
+header('Expires: 0');?>
+
 <!doctype html>
 <html <?php language_attributes(); ?>>
 
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png" type="image/x-icon" />
     <link rel="profile" href="https://gmpg.org/xfn/11">
 
     <?php wp_head(); ?>
